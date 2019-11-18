b2ebd412ae72b488fc52edf81dd6142c5a877173
tyler.chen <M_chenjiafeng@163.com>
2019-11-18 23:55:06 +0800

    通过id单独查询

--- mybatis_day02_1_crud/src/test/java/com/itheima/TestMybatiesCURD.java
+++ mybatis_day02_1_crud/src/test/java/com/itheima/TestMybatiesCURD.java
@@ -14,7 +14,9 @@ import java.util.List;
  */
 
 public class TestMybatiesCURD {
-
+    /**
+     * 查询所有
+     */
     @Test
     public void testFindAll(){
 
@@ -28,7 +30,7 @@ public class TestMybatiesCURD {
         //获取Sqlsession对象
        SqlSession sqlSession= sqlSessionFactory.openSession();
 
-       //执行查询（userMapper--->namespaced的，findAll-->id）
+       //执行查询（UserMapper.xml的userMapper--->namespaced的，findAll-->id）
       List<User> userList= sqlSession.selectList("userMapper.findAll");
       for (User users:userList){
           System.out.println(users);
@@ -39,4 +41,26 @@ public class TestMybatiesCURD {
     }
 
 
+    /**
+     * 根据用户id查询
+     */
+    @Test
+    public  void  testFindById(){
+        //获取输入流对象
+        InputStream inputStream= this.getClass().getClassLoader().getResourceAsStream("sqlMapConfig.xml");
+
+        //获取SqlSessFactory(sqlsessiond的工厂类)的对象
+
+        SqlSessionFactory sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
+
+        //获取Sqlsession对象
+        SqlSession sqlSession= sqlSessionFactory.openSession();
+
+        //查询sql
+       List<User> user= sqlSession.selectList("userMapper.findById",1);
+        for(User users: user){
+            System.out.println("单独查询"+users);
+        }
+        sqlSession.close();
+    }
 }
