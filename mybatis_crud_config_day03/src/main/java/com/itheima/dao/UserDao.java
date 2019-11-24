7e7c4825372bd2772877da566fa73d36914875d6
tyler.chen <M_chenjiafeng@163.com>
2019-11-24 23:12:13 +0800

    数据库事务管理

---
+++ mybatis_crud_config_day03/src/main/java/com/itheima/dao/UserDao.java
@@ -0,0 +1,36 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+
+import java.util.List;
+
+/**
+ * @author tyler.chen
+ * @date 2019/11/20 21:53
+ */
+public interface UserDao {
+    /**
+     * 查询全部
+     * @return
+     */
+    public List<User> findAll();
+
+    /**
+     * 插入用户数据
+     * @param user
+     */
+    public void save(User user);
+
+
+    /**
+     * 更细数据
+     * @param user
+     */
+    public  void update(User user);
+
+    /**
+     * 根据id删除用户
+     * @param id
+     */
+    public void delete(Integer id);
+}
